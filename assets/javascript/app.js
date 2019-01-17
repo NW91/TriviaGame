08f4d543a09fdcdd8b8e7539f2ef7c5e8b1bb3e8
Nicole W <nicoleewhitee@gmail.com>
2019-01-16 20:55:31 -0800

    psuedocode + broken loaded question function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
-$("start").on('click', function(){
+//Javascript
+$("start").on("click", function(){
     $("#start").remove();
+    game.loadQuestion();
 })
 
+//trivia questions array, sub-answers array, and images to attach
 var questions = [{
     question: "what city is not a part of the PNW example question?",
     answers: ["Portland", "Seattle", "Los Angeles", "Cannon Beach"],
@@ -79,24 +82,33 @@ var questions = [{
     image: "assets/images/choiceNumber1.gif"
 }];
 
+//variables
 var game = {
     questions: question,
     currentQuestion: 0,
     counter: 30,
     correct: 0,
     incorrect: 0,
+
+//functions
     countdown: function(){
-
+        game.counter--;
+        $("#counter").html(game.counter);
+        if(game.counter <= 0 ) {
+            console.log("Time's up!");
+            game.timeUp();
+        }
     },
-
     loadQuestion: function(){
-
+        time = setInterval(game.countdown, 1000);
+        $("#subwrapper").html("<h2>" + questions[game.currentQuestion].question + "<h2>");
+        for(var i = 0; i < questions[game.currentQuestion].answers.length; i++){
+            $("#subwrapper").append('<button-class = "answer-button" id = "button-' + i + '" data-name="' + questions[game.currentQuestion].answers[i] + '">' + questions[game.currentQuestion].answers[i] + '</button>');
+        }
     },
-
     nextQuestion: function(){
 
     },
-
     timeUp: function(){
 
     },
@@ -104,21 +116,17 @@ var game = {
     results: function(){
 
     },
-    
     clicked: function(){
 
     },
-
     answerCorrectly: function(){
 
     },
-
     answeredIncorrectly: function(){
 
     },
-
     reset: function(){
         
     }
 
-}
+};
