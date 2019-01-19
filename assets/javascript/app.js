8990d363bb7d2a7f141f8e522117bfa6f276f33f
Nicole W <nicoleewhitee@gmail.com>
2019-01-19 10:33:22 -0800

    functions logic added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,10 @@ $(document).on('click', '.answer-button', function(e){
     game.clicked(e);
 })
 
+$(document).on('click', '#reset', function(){
+    game.reset();
+})
+
 //trivia questions array, sub-answers array, and images to attach
 var questions = [{
     question: "In the Little Mermaid, what is the name of the Seagull that makes up stories about human objects?",
@@ -104,7 +108,8 @@ var game = {
     },
     loadQuestion: function(){
         timer = setInterval(game.countdown,1000);
-        $('#subwrapper').html('<h2>'+questions[game.currentQuestion].question+'</h2>');
+        $('#subwrapper').html("<h2>'counter'>30</h2>");
+        $('#subwrapper').append('<h2>'+questions[game.currentQuestion].question+'</h2>');
         for(var i=0;i<questions[game.currentQuestion].answers.length;i++){
             $('#subwrapper').append('<button class="answer-button" id="button-'+i+'" data-name="'+questions[game.currentQuestion].answers[i]+'">'+questions[game.currentQuestion].answers[i]+'</button>');
             console.log(timer)
@@ -112,28 +117,66 @@ var game = {
     },
     nextQuestion: function(){
         game.counter = 30;
-        $("#counter").html(game.counter);
+        $('#counter').html(game.counter);
         game.currentQuestion++;
         game.loadQuestion();
     },
     timeUp: function(){
-
+        clearInterval(timer);
+        game.unanswered++;
+        $('#subwrapper').html('<h2>You are out of time!</h2>');
+        $('#subwrapper').append('<h3>The Correct Answer Is: '+questions[game.currentQuestion].correctAnswer+'</h3>');
+        if(game.currentQuestion==questions.length-1){
+            setTimeout(game.results,3*1000);
+        } else {
+            setTimeout(game.nextQuestion, 3*1000);
+        }
     },
-
     results: function(){
-
+        clearInterval(timer);
+        $('#subwrapper').html("<h2>All Done!</h2>");
+        $('#subwrapper').append("<h3>Correct: "+game.correct+"</h3>");
+        $('#subwrapper').append("<h3>Incorrect "+game.incorrect+"</h3>");
+        $('#subwrapper').append("<h3>Unanswered: "+game.unanswered+"</h3>");
+        $('#subwrapper').append("<button id='reset'>Reset</button>")
     },
     clicked: function(){
-
+        clearInterval(timer);
+        if($(e.target).data("name")==questions[game.currentQuestion].correctAnswer){ game.answeredCorrectly();
+        } else {
+            game.answeredIncorrectly();
+        }
     },
     answerCorrectly: function(){
-
+        console.log("You got it right!");
+        clearInterval(timer);
+        game.correct++;
+        $('#subwrapper').html('<h2>Congratulations, you got it right!<h2>');
+        if(game.currentQuestion==questions.length-1){
+            setTimeout(game.results,3*1000);
+        } else {
+            setTimeout(game.nextQuestion, 3*1000);
+        }
     },
     answeredIncorrectly: function(){
-
+        console.log("Sorry, that's the wrong answer!")
+        clearInterval(timer);
+        game.correct++;
+        $('#subwrapper').html('<h2>Sorry, this is the wrong answer!<h2>');
+        $('#subwrapper').append('<h3>The Correct Answer Is: '+questions[game.currentQuestion].correctAnswer+'</h3>');
+        if(game.currentQuestion==questions.length-1){
+            setTimeout(game.results,3*1000);
+        } else {
+            setTimeout(game.nextQuestion, 3*1000);
+        }
     },
     reset: function(){
-        
+        game.currentQuestion = 0;
+        game.counter = 0;
+        game.correct = 0;
+        game.incorrect = 0;
+        game.unanswered = 0;
+        game.loadQuestion();
     }
 
 }
